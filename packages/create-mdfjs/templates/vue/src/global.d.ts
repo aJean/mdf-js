3212680d4ed92ba4533ecfee2a58bec56c612219
qy <30001677@qq.com>
2021-04-13 16:36:46 +0800

    update

---
+++ packages/create-mdfjs/templates/vue/src/global.d.ts
@@ -0,0 +1,46 @@
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
+
+declare module '*.css?module' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss?module' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.less?module' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.sass?module' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
